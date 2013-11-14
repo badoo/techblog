446e8fcbff78de6780f79071556b3c9cacb5a803
William Lewis <william.lewis@corp.badoo.com>
2013-11-14 17:54:00 +0000

    fix to_liquid override for latest gem

--- _plugins/image.rb
+++ _plugins/image.rb
@@ -1,11 +1,13 @@
-class Jekyll::Post
-	alias :to_liquid_without_comments :to_liquid
+module Jekyll
+    class Jekyll::Post
+        alias :to_liquid_without_comments :to_liquid
 
-	def to_liquid()
-		data = to_liquid_without_comments()
-		data['imgdir'] = '/images/' + data['url'].split('/').pop
-		data['demodir'] = '/demo/' + data['url'].split('/').pop
-		data['filesdir'] = '/files/' + data['url'].split('/').pop
-		data
-	end
+        def to_liquid(attrs = ATTRIBUTES_FOR_LIQUID)
+            data = to_liquid_without_comments(attrs)
+            data['imgdir'] = '/images/' + data['url'].split('/').pop
+            data['demodir'] = '/demo/' + data['url'].split('/').pop
+            data['filesdir'] = '/files/' + data['url'].split('/').pop
+            data
+        end
+    end
 end
